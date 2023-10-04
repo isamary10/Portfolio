3920e4a34bb47a44a7d99ed1fb21d9ba97d6ea52
Isa Mary <isa.kusuki@gmail.com>
2023-10-03 22:07:37 -0300

    Criação do Projeto

---
+++ Portfolio/Controllers/ProjectController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+
+namespace Portfolio.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private static List<Project> _list = new List<Project>();
+        private static int _cont = 0;
+
+        public IActionResult Index()
+        {
+            return View(_list);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Project project)
+        {
+            project.Id = ++_cont;
+            _list.Add(project);
+            TempData["msg"] = "Project created successfully";
+            return RedirectToAction("Create");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portfolio/Models/Project.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        //public string? ImageUrl { get; set; }
+        [DataType(DataType.Date), Display(Name = "Start Date")]
+        public DateTime? StartDate { get; set; }
+        [DataType(DataType.Date), Display(Name = "End Date")]
+        public DateTime? EndDate { get; set; }
+        [Display(Name = "Technologies Used")]
+        public string? TechnologiesUsed { get; set; }
+        [Display(Name = "Projet URL")]
+        public string? ProjectUrl { get; set; }
+        public IFormFile ImageFile { get; set; }
+        public Category Category { get; set; }
+       
+    }
+
+    public enum Category
+    {
+        [Display(Name = "Backend Development")]
+        BackendDevelopment,
+        Cloud,
+        [Display(Name = "Data Analysis")]
+        DataAnalysis,
+        Database,
+        [Display(Name = "Frontend Development")]
+        FrontendDevelopment,
+        [Display(Name = "Graphic Design")]
+        GraphicDesign,
+        [Display(Name = "Mobile Development")]
+        MobileDevelopment,
+        [Display(Name = "UI UX")]
+        UiUx,
+        [Display(Name = "Web Development")]
+        WebDevelopment
+    }
+
+}
